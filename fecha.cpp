fd64ecffd605f6d5c9c58c3f2e19c6fe01aeb02b
Daniel Martinez Lozano <84764267+Danielmartinez25@users.noreply.github.com>
2023-09-27 22:30:41 -0300

    fecha.cpp

---
+++ fecha.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+class Fecha
+{
+private:
+    int Dia, Mes, Anio;
+
+public:
+    Fecha(int d = 1, int m = 1, int a = 2000)
+    {
+        Dia = d;
+        Mes = m;
+        Anio = a;
+    }
+    void setDia(int d) { Dia = d; }
+    void setMes(int m) { Mes = m; }
+    void setAnio(int a) { Anio = a; }
+    int getDia() { return Dia; }
+    int getMes() { return Mes; }
+    int getAnio() { return Anio; }
+    void Cargar();
+    void Mostrar();
+};
+
+void Fecha::Cargar()
+{
+    cout << "Ingresa un dia" << endl;
+    cin >> Dia;
+    cout << "Ingresa un mes" << endl;
+    cin >> Mes;
+    cout << "Ingresa un anio" << endl;
+    cin >> Anio;
+}
+
+void Fecha::Mostrar()
+{
+    cout << "Dia: " << Dia << endl;
+    cout << "Mes: " << Mes << endl;
+    cout << "Anio: " << Anio << endl;
+}
+int main()
+{
+    Fecha fec;
+    fec.Cargar();
+    fec.Mostrar();
+    return 0;
+}
